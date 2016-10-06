1adaee2e9f83b06bdfa7a26dd65a59d89acbf0c9
Murezhi <murezhi@gmail.com>
2016-10-06 10:20:47 +0530

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,80 @@
+package web.murezhi.com.webapp;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.ShareActionProvider;
+
+
+
+
+public class MainActivity extends Activity {
+
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWebView = (WebView)findViewById(R.id.activity_main_webview);
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mWebView.setWebViewClient(new web.murezhi.com.webapp.MyAppWebViewClient(this)
+
+       {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                //hide loading image
+                findViewById(R.id.progressBar1).setVisibility(View.GONE);
+                //show webview
+                findViewById(R.id.activity_main_webview).setVisibility(View.VISIBLE);
+            }});
+
+        mWebView.loadUrl("http://murezhi.wordpress.com");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+    private ShareActionProvider mShareActionProvider;
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        /** Inflating the current activity's menu with res/menu/items.xml */
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        /** Getting the actionprovider associated with the menu item whose id is share */
+        mShareActionProvider = (ShareActionProvider) menu.findItem(R.id.share).getActionProvider();
+
+        /** Setting a share intent */
+        mShareActionProvider.setShareIntent(getDefaultShareIntent());
+
+        return super.onCreateOptionsMenu(menu);
+
+    }
+
+    /** Returns a share intent */
+    private Intent getDefaultShareIntent(){
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Convert Website to Android Application");
+        intent.putExtra(Intent.EXTRA_TEXT," Vist www.AndroidWebViewApp.com if you Want to Convert your Website or Blog to Android Application");
+        return intent;
+    }
+
+
+}
